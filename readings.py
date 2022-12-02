7d971413c3b3319f3b36c9aecef2e0b9b4697978
David Nichol <shekelator@gmail.com>
2022-12-01 22:33:48 -0500

    Add some functionality, tests, gitignore

--- readings.py
+++ readings.py
@@ -5,17 +5,33 @@ import json
 class Service:
     def fromDict(self, d):
         self.date =  datetime.datetime.strptime(d["date"], "%Y-%m-%d").date()
+        self.name = d["name"]["en"]
+        self.isShabbat = "7" in d["fullkriyah"]
+        self.readings = {k: self.convertReading(v) for k, v in d["fullkriyah"].items()}
         return self
 
-def getReadings():
+    def convertReading(self, readingData):
+        book = readingData["k"]
+        begin = readingData["b"]
+        end = readingData["e"]
+        return f"{book} {begin}-{end}"
+
+
+def getRawReadingsData():
     date = datetime.datetime.now()
     endDate = date + datetime.timedelta(days=180)
     date.strftime("%d-%m-%Y")
     url = f"https://www.hebcal.com/leyning?cfg=json&start={date.strftime('%Y-%m-01')}&end={endDate.strftime('%Y-%m-%d')}"
     data = requests.get(url)
-    print(url)
+    rawDict = json.loads(data.text)
+    return data["items"]
 
-    return json.loads(data.text)
+def getReadings(rawItems):
+    shabbatot = filter(lambda r: 'fullkriyah' in r, rawItems)
+    shabbatServices = map(lambda i: Service().fromDict(i), shabbatot)
+    
+    return shabbatServices
+
     
 
 if __name__ == "__main__":
--- test_readings.py
+++ test_readings.py
@@ -20,3 +20,13 @@ class TestReadings:
         services = list(map(lambda x: readings.Service().fromDict(x), hebCalData))
         assert services[0].date == datetime.date(2022, 11, 3)
         assert services[1].date == datetime.date(2022, 11, 5)
+        assert services[1].name == "Lech-Lecha"
+
+    def test_can_get_services(self, hebCalData):
+        services = list(readings.getReadings(hebCalData))
+        assert services[0].name == "Lech-Lecha"
+        assert services[0].date == datetime.date(2022, 11, 5)
+        assert services[0].isShabbat == True
+        assert services[0].readings["1"] == "Genesis 12:1-12:13"
+        assert services[0].readings["3"] == "Genesis 13:5-13:18"
+
